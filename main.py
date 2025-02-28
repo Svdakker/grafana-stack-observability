02ec3c76f9e089333ff15283c1c7040f3b8bb179
Simon van den Akker <simon.van.den.akker@knmi.nl>
2025-02-28 20:33:12 +0100

    Implemented prometheus metrics to alloy

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import logging
 import time
+from opentelemetry import metrics
 from opentelemetry.sdk.resources import Resource
 from opentelemetry.exporter.otlp.proto.grpc._log_exporter import OTLPLogExporter
+from opentelemetry.exporter.otlp.proto.grpc.metric_exporter import OTLPMetricExporter
 from opentelemetry.sdk._logs import LoggerProvider, LoggingHandler
 from opentelemetry.sdk._logs.export import BatchLogRecordProcessor
+from opentelemetry.sdk.metrics.export import PeriodicExportingMetricReader
+from opentelemetry.sdk.metrics import MeterProvider
 
 
 # Define a custom resource to monitor
@@ -17,7 +21,7 @@ resource = Resource(
 
 # Setup basic logging
 logging.basicConfig(
-    level=logging.INFO,
+    level=logging.DEBUG,
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
 )
 logger = logging.getLogger(__name__)
@@ -28,14 +32,31 @@ log_exporter = OTLPLogExporter(endpoint="http://localhost:4317", insecure=True)
 log_provider.add_log_record_processor(BatchLogRecordProcessor(log_exporter))
 logger.addHandler(LoggingHandler(logger_provider=log_provider))
 
+# Set up OpenTelemetry metrics
+metric_reader = PeriodicExportingMetricReader(
+    OTLPMetricExporter(endpoint="http://localhost:4317", insecure=True),
+)
+metrics_provider = MeterProvider(resource=resource, metric_readers=[metric_reader])
+metrics.set_meter_provider(meter_provider=metrics_provider)
+meter = metrics.get_meter(__name__)
+counter = meter.create_counter(
+    name="test_counter",
+    description="Counts in increments of 1",
+    unit="1"
+)
+
 def main():
+    count = 0
     try:
         while True:
             logger.info("Running...")
-            logger.info("Sleeping for 5 seconds...")
+            count += 1
+            counter.add(1)
+            logger.info(f"Counter: {count}")
             time.sleep(5)
     except KeyboardInterrupt:
         logger.info("Shutting down...")
+        metrics_provider.shutdown()
         log_provider.shutdown()
 
 if __name__ == "__main__":
